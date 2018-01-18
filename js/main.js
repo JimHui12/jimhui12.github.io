a38b4c602f58604290c5867f287de4f3d45abad8
idea-PC\jim <jianghua.hui@uq.net.au>
2018-01-18 21:01:22 +1000

    main.js Portfolio subpage filters function added

--- js/main.js
+++ js/main.js
@@ -5,4 +5,46 @@
         var subpagesHeight = $('.pt-page-current').height();
         $(".subpages").height(subpagesHeight + 50);
     }
+
+    // Portfolio subpage filters
+    function protfolio_init() {
+        var protfolio_grid = $('#portfolio_grid'),
+        protfolio_filter = $('#portfolio_filters');
+
+        if (protfolio_grid) {
+            protfolio_grid.shuffle({
+                speed: 450,
+                itemSelector: 'figure'
+            });
+
+            $('.site-main-menu').on("click", "a", function (e) {
+                protfolio_grid.shuffle('update');
+            });
+
+            protfolio_filter.on("click", ".filter", function (e) {
+                protfolio_grid.shuffle('update');
+                e.preventDefault();
+                $('#portfolio_filters .filter').parent().removeClass('active');
+                $(this).parent().addClass('active');
+                protfolio_grid.shuffle('shuffle', $(this).attr('data-group') );
+                setTimeout(function(){
+                    subpages_resize();
+
+                }, 500);
+            });
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
