62069c944c9dad32374d4d65443291fa49937374
idea-PC\jim <jianghua.hui@uq.net.au>
2018-01-19 21:04:00 +1000

    main.js Text rotation function added

--- js/main.js
+++ js/main.js
@@ -164,6 +164,21 @@ $(document).on('ready', function() {
         }
    });
 
+   // Text rotation
+   $('.text-rotation').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: false,
+        margin: 0,
+        items: 1,
+        autoplay: true,
+        autoplayHoverPause: false,
+        autoplayTimeout: 3800,
+        animateOut: 'zoomOut',
+        animateIn: 'zoomIn'
+    }); 
+
+    
 
 
 
