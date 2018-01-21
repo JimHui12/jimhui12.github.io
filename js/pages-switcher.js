3771779102f55210153fc4287bd62c1d798a19bb
idea-PC\jim <jianghua.hui@uq.net.au>
2018-01-21 21:17:19 +1000

    pages-switcher.js PageTransitions added

--- js/pages-switcher.js
+++ js/pages-switcher.js
@@ -5,3 +5,20 @@
 * Version: 1.0
 */
 
+var PageTransitions = (function ($, options) {
+    "use strict";
+    var defaultStartPage = "home",
+    sectionContainer = $(".subpages"),
+    isAnimating = false,
+    endCurrentPage = true,
+    endNextPage = false,
+    windowArea = $(window),
+    animEndEventNames = {
+        'WebkitAnimation'   : 'webkitAnimationEnd',
+        'OnAnimation'       : 'oAnimationEnd',
+        'msAnimation'       : 'MsAnimationEnd',
+        'animation'         : 'animationend'
+    },
+
+    
+})
